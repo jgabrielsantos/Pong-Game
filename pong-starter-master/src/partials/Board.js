4b67393611c35e3405be534a5c096797c4077ff0
Joao Gabriel Carvalho <joaoocarvalhol.santos@gmail.com>
2019-11-05 20:49:11 -0800

    add board and middle line to js

--- pong-starter-master/src/partials/Board.js
+++ pong-starter-master/src/partials/Board.js
@@ -5,22 +5,22 @@ export default class Board {
         this.height = height;
     }
 
-    render() {
+    render(svg) {
         let board = document.createElementNS(SVG_NS, "rect");
-        board.setAttribute(null, "x", 0);
-        board.setAttribute(null, "y", 0);
-        board.setAttribute(null, "width", this.width);
-        board.setAttribute(null, "height", this.height);
-        board.setAttribute(null, "fill", B_PROPERTIES.color);
+        board.setAttributeNS(null, "x", 0);
+        board.setAttributeNS(null, "y", 0);
+        board.setAttributeNS(null, "width", this.width);
+        board.setAttributeNS(null, "height", this.height);
+        board.setAttributeNS(null, "fill", B_PROPERTIES.color);
 
         let line = document.createElementNS(SVG_NS, "line");
-        line.setAttribute(null, "x1", this.width / 2);
-        line.setAttribute(null, "x2", this.width / 2);
-        line.setAttribute(null, "y1", 0);
-        line.setAttribute(null, "y2", this.height);
-        line.setAttribute(null, "stroke", L_PROPERTIES.color);
-        line.setAttribute(null, "stroke-dasharray", L_PROPERTIES.dasharray);
-        line.setAttribute(null, "stroke-width", L_PROPERTIES.stroke_width);
+        line.setAttributeNS(null, "x1", this.width / 2);
+        line.setAttributeNS(null, "x2", this.width / 2);
+        line.setAttributeNS(null, "y1", 0);
+        line.setAttributeNS(null, "y2", this.height);
+        line.setAttributeNS(null, "stroke", L_PROPERTIES.color);
+        line.setAttributeNS(null, "stroke-dasharray", L_PROPERTIES.dasharray);
+        line.setAttributeNS(null, "stroke-width", L_PROPERTIES.stroke_width);
 
         svg.appendChild(board);
         svg.appendChild(line);
